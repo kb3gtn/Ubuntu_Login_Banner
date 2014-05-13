7dc4c05b58ae315489df756bdb22ebc27ce6bf25
Peter Fetterer <kb3gtn@gmail.com>
2014-05-13 00:15:42 -0400

    add install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing pop up banner.  To disable, edit /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf" 
-sudo cp /usr/share/lightdm.conf.d/50-ubuntu.conf /usr/share/lightdm/lightdm.conf.d/50-ubuntu.backup
+sudo cp /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf /usr/share/lightdm/lightdm.conf.d/50-ubuntu.backup
 sudo cp 50-ubuntu.conf /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
 sudo cp login_banner.html /etc/
 sudo cp banner_popup_pyqt5.py /usr/bin
