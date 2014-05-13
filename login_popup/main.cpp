37abc9cbc11aad57b2ef1eec683ae123f1a8656a
Peter Fetterer <kb3gtn@gmail.com>
2014-05-13 01:44:59 -0400

    updated main to show widget in middle of screen, add bin for ubuntu 14.04 x86-64

--- login_popup/main.cpp
+++ login_popup/main.cpp
@@ -1,5 +1,6 @@
 #include "loginbanner.hpp"
 #include <QApplication>
+#include <QDesktopWidget>
 #include <iostream>
 
 int main(int argc, char *argv[])
@@ -7,6 +8,8 @@ int main(int argc, char *argv[])
     int ret;
     QApplication a(argc, argv);
     LoginBanner w;
+    w.adjustSize();
+    w.move( QApplication::desktop()->screen()->rect().center() - w.rect().center() );
     w.show();
     a.exec();
     ret = w.result();
